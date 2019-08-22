26fcbe8a7cd80029bf7ab49061dc1eb01b477b47
Nevin Jojo <nevinjojo98@gmail.com>
2019-08-22 14:07:56 +1200

    Initial Commit

---
+++ Allocated/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Allocated
+//
+//  Created by Nevin Jojo on 21/08/19.
+//  Copyright © 2019 Nevin Jojo. All rights reserved.
+//
+
+import UIKit
+
+struct Category {
+    var opened = Bool()
+    var title = String()
+    var categoryData = [String]()
+}
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    //MARK: Properties
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
